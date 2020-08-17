a7c48fa0b86652d8b0c0e4ab9fad1485ca027921
Joao Victor <joaovictorcsantos13@gmail.com>
2020-08-17 17:08:57 -0300

    initial commit

---
+++ services/api-routes.js
@@ -0,0 +1,9 @@
+import { api } from './api'
+export const api_key = 'a609aef1'
+
+
+export default {
+    searchFilm:(filmName) => {
+        return api.get(`/?apikey=${api_key}&s=${filmName}`)
+    }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    <header id="header-content">
+      <input type="text" placeholder="Buscar por um Filme" v-model="filmName" />
+      <button v-on:click="searchFilm(filmName)" @keyup.enter="searchFilm(filmName)">Pesquisar</button>
+    </header>
+    <div class="list-content">
+      <div class="content-box">
+        <ul v-for="film in films" :key="film.id">
+          <img :src="film.Poster"/>
+          <p>{{film.Title}}</p>          
+          <p>{{film.Year}}</p>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import apiRoutes from "../services/api-routes";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => {
+    return {
+      films: [],
+      filmName: "",
+    };
+  },
+  methods: {
+    async searchFilm(filmName) {
+      const res = await apiRoutes.searchFilm(filmName);
+      const { Search } = res.data;
+      console.log(res.data);
+      Search.forEach((value) => {
+        this.films.push(value);
+      });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+
+ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
 }
+
+img{
+    width: 80px;    
+    margin-right: 10px;
+    border-radius: 15%;
+}
+
+p{
+    margin: 5px 0;
+}
+
 </style>
